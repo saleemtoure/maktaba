84abd87baf7816137110ff1e38e0de3ba943c768
Saleem Toure <saleemtoure@gmail.com>
2024-12-19 23:01:54 +0100

    Updated design for test

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 // script.js
 document.getElementById('changeTextButton').addEventListener('click', function () {
-    document.getElementById('header').textContent = 'You clicked the button!';
+    document.getElementById('header').textContent = '!السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ';
 });
