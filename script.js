65684188013dcf4d3b652712be88a8e2036c754a
Saleem Toure <saleemtoure@gmail.com>
2024-12-19 22:54:21 +0100

    Added a test index, styles, script set up

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById('changeTextButton').addEventListener('click', function () {
+    document.getElementById('header').textContent = 'You clicked the button!';
+});
